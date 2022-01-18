0833c5d267a62f2bee3b981167bd6a306f071cdc
Salabon <sergeistarshark@mail.ru>
2022-01-18 20:24:39 +0300

    create python

---
+++ Lesson 1/hello.py
@@ -0,0 +1,55 @@
+# print('Hello')
+# a = 123
+
+# b = 1.243
+
+# s = 'строка'
+
+# print(a, '-',b, '-',s)
+# print(f'{a}-{b}-{s}')
+
+
+# print('введите а')
+# a = int(input()) # int() это чтобы распознавал не как строку, а как число
+# print('введите b')
+# b = int(input())
+# print(a,'+',b,'=',a+b)
+
+
+# a = 1.3123123
+# b = 3
+# c = round(a * b, 5) # округление вещественного числа, 5 знаков после запятой
+# print(c)
+
+# f = 1 > 2 or 4 < 6
+# print(f)
+
+# f = [1, 2, 3, 4]
+# print(f)
+# print(2 in f)
+# print(not 2 in f)
+# is_odd = not f[0] % 2 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>РАЗОБРАТЬСЯ
+# print(is_odd)
+
+# a = int(input('a = '))                   Оператор if else
+# b = int(input('b = '))
+
+# if a>b:
+#     print(a)
+# else:
+#     print(b)
+
+# for i in range(5):
+#     print(i)
+# text = 'какой то текст'
+
+# ======================================= ФУНКЦИИ
+def f(x):
+    if x == 1:
+        return 'целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return 0
+
+
