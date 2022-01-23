78df594f71ebcabfa5f711470700ae3a748719f9
Salabon <sergeistarshark@mail.ru>
2022-01-23 22:26:55 +0300

    Optimized Task 11

--- Tasks/Task 11.py
+++ Tasks/Task 11.py
@@ -1,8 +1,9 @@
 # 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 
 import random
-a = random.randint(1, 99)
+a = random.randint(10, 99)
 print(f'Рандомное число = {a}')
+
 def WhoMore(a):
     first_num = a // 10
     secont_num = a % 10
@@ -12,3 +13,16 @@ def WhoMore(a):
     else:
         return secont_num
 print(f'наибольшая цифра = {WhoMore(a)}')   
+
+# =========== 2 вариант решения ===========
+
+import random
+a = random.randint(10, 99)
+print(f'Рандомное число = {a}')
+def WhoMore2(num):   
+    a = str(num)
+    if a[0] > a[1]:
+        return a[0]
+    else:
+        return a[1]
+print(f'Наибольшая цифра из числа {a} это {WhoMore2(a)}')
