b6b76485d824c9d87e24c0480ae60e691a159748
Salabon <sergeistarshark@mail.ru>
2022-01-26 07:10:33 +0300

    26 01 morning

---
+++ Lesson 2/1.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+
+# with open('file.txt', 'a') as data:
+#     data.write('line 1123\n')
+#     data.write('line 2123\n')
+
+# colors = {'red', 'green', 'blue'}
+# data = open('file.txt', 'a')
+# data.writelines(colors) # разделителей не будет
+# data.write('\nLINE_2\n')
+# data.write('LINE_3\n')
+# data.close()
+
+
+# path = 'file.txt'
+# data = open(path,'r')
+# for line in data:
+#     print(line)
+# data.close
+# exit()
+import hello as h
+print(h.f(1))
