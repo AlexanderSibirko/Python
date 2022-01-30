fade3d072633aee19c2a1be9aa05a2d8e161408c
Salabon <sergeistarshark@mail.ru>
2022-01-30 08:28:42 +0300

    add new functions and update knowlidge base

---
+++ Functions.py
@@ -0,0 +1,21 @@
+# ====================== ПОЛЕЗНЫЕ ФУНКЦИИ =====================
+
+# def Method(*params):                 Суммирует все введенные параметры, сколько захотим
+#     res = 0
+#     for item in params:
+#         res += item
+#     return res
+# print(Method(1,2,3,4,5,6))
+
+# import random
+# def RandomNum():
+#     a = random.randint(1, 10)
+#     print(f'Рандомное число = {a}')
+# RandomNum()
+
+
+
+
+r = open('file.txt', 'w')
+r.write('каждый охотник желает знать где сидит фазан')
+print()
