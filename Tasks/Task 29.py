959d0201c87f39e21ec92f4d89b64809b36c3f44
Salabon <sergeistarshark@mail.ru>
2022-02-03 07:20:39 +0300

    03 02 22

--- Tasks/Task 29.py
+++ Tasks/Task 29.py
@@ -1,13 +1,17 @@
 # 29. Написать программу вычисления произведения чисел от 1 до N
 
-def ProizvedDoN(num):
-    num = int(num)
-    proiz = 1
-    count = 1
-    while count <= num:
-        proiz *= count
-        count += 1
-    return (f'Произведение чисел от 1 до {num} = {proiz}')
+# def ProizvedDoN(num):
+#     num = int(num)
+#     proiz = 1
+#     count = 1
+#     while count <= num:
+#         proiz *= count
+#         count += 1
+#     return (f'Произведение чисел от 1 до {num} = {proiz}')
 
-print('Введите число N: ')
-print(ProizvedDoN(input()))
+# print('Введите число N: ')
+# print(ProizvedDoN(input()))
+
+
+
+
