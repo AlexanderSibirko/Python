0224cfd5cd37255afcb8a806207022e84fa4a566
Salabon <sergeistarshark@mail.ru>
2022-02-06 09:29:26 +0300

    add task 19

--- Tasks/Task 19.py
+++ Tasks/Task 19.py
@@ -1,14 +1,12 @@
-# 19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+# 19. Реализовать алгоритм задания случайных чисел. Без использования встроенного генератора псевдослучайных чисел
 
-def NumberChetvert(x,y):
-    print(f'\n Координата х = {x}\n')
-    print(f' Координата y = {y}\n')
-    if (x > 0) and (y > 0):
-        return 1
-    elif ((x < 0) and (y > 0)):
-        return 2
-    elif ((x < 0) and (y < 0)):
-        return 3
-    elif ((x > 0) and (y < 0)):
-        return 4
-print(f'Точка находится в {NumberChetvert(-2,-3)} четверти\n')
+
+set_ = set()
+for i in range(0,11):
+    set_.add((str(i)))     
+    
+list = []
+for i in set_:
+    iint = int(i)
+    list.append(iint)
+print(list)
