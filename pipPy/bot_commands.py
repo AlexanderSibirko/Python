6b591b4db2df834220fdd00101e75f5fa594ebb5
Salabon <sergeistarshark@mail.ru>
2022-02-20 11:43:40 +0300

    работаем над игрой

--- pipPy/bot_commands.py
+++ pipPy/bot_commands.py
@@ -7,6 +7,10 @@ def hi_command(update: Update, context: CallbackContext):
     log(update, context)
     update.message.reply_text(f'Zdarova {update.effective_user.first_name}!')
 
+list_gamers = []
+
+        
+
 def help_command(update: Update, context: CallbackContext):
     log(update, context)
     update.message.reply_text(f'/hi\n/time\n/help\n/sum')
@@ -21,5 +25,20 @@ def sum_command(update: Update, context: CallbackContext):
     items = msg.split()  #/sum 41234 1234123
     x = int(items[1])
     y = int(items[2])
-
     update.message.reply_text(f'{x} + {y} = {x + y}')
+
+def candy_join(update: Update, context: CallbackContext):
+    global list_gamers
+    current_user = update.effective_user.id
+    if current_user not in list_gamers:
+        list_gamers.append(current_user)
+        log(update, context)
+        context.bot.send_message(current_user, 'Теперь Вы в игре')
+        for user_id in list_gamers:
+            context.bot.send_message(user_id, f'всего {len(list_gamers)} игроков')
+
+def start_game(update: Update, context: CallbackContext):
+    current_user = update.effective_user.id
+    if current_user == list_gamers[0]:
+        for user_id in list_gamers:
+            context.bot.send_message(user_id, f'Игра началась\nВсего 100 конфет. Каждый игрок по очереди берет не более 10 конфет.\n Ходит игрок по имени {}')
--- pipPy/main.py
+++ pipPy/main.py
@@ -1,35 +1,6 @@
-# from isOdd import isOdd
-
-# print(isOdd(1))
-# print(isOdd(4)) 
-
-
-# from progress.bar import Bar
-# import time
-# bar = Bar('Processing', max=20)
-# for i in range(20):
-#     time.sleep(1)
-#     bar.next()
-# bar.finish()
-
-
-# import emoji
-# print(emoji.emojize('Python is :thumbs_up:'))
-
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# list = [1,2,3,2,7]
-# plt.plot(list)
-
-# plt.show()
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from bot_commands import *
-
-
-
-
 
 
 updater = Updater('5153530856:AAFo2poFM33Hkt6xvp6rQOUXVNbSZ9KPSg0')
@@ -38,6 +9,8 @@ updater.dispatcher.add_handler(CommandHandler('hi', hi_command))
 updater.dispatcher.add_handler(CommandHandler('time', time_command))
 updater.dispatcher.add_handler(CommandHandler('help', help_command))
 updater.dispatcher.add_handler(CommandHandler('sum', sum_command))
+updater.dispatcher.add_handler(CommandHandler('game', candy_join))
+updater.dispatcher.add_handler(CommandHandler('start_game', start_game))
 
 print('server start')
 updater.start_polling()
